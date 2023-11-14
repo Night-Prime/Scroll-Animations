d7d8096db75e02a6ba45e5fec070b258c1f97fca
Daniel Abati <danielabatibabatunde1@gmail.com>
2023-11-14 21:35:34 +0100

    figured-scroll

--- src/App.js
+++ src/App.js
@@ -1,153 +1,146 @@
 import { useRef, useLayoutEffect } from "react";
 import "./App.css";
-import Hero from "./components/Hero";
 import gsap from "gsap";
 import ScrollTrigger from "gsap/src/ScrollTrigger";
 
+/**
+ * @summary Practicing how to use GSAP ScrollTrigger in Horizontal view
+ * @returns Smooth Web Motion
+ */
+
 function App() {
-  const app = useRef();
+  const triggerRef = useRef(null);
+  const sectionRef = useRef(null);
 
   gsap.registerPlugin(ScrollTrigger);
+
   useLayoutEffect(() => {
-    gsap.defaults({
-      duration: 2,
-      // repeatDelay: 0.5,
-      // repeatRefresh: true,
-      autoAlpha: 1,
-    });
-    let ctx = gsap.context(() => {
+    const pin = gsap.context(() => {
       gsap
         .timeline()
         .fromTo(
-          ".box h1",
+          sectionRef.current,
           {
-            opacity: 0,
-            y: -200,
+            translateX: 0,
           },
           {
-            opacity: 1,
-            y: 0,
-            stagger: "each",
+            translateX: "-500vw",
+            ease: "none",
+            duration: 1,
+            scrollTrigger: {
+              trigger: triggerRef.current,
+              start: "top top",
+              end: "bottom top",
+              scrub: 0.6,
+              pin: true,
+            },
           }
         )
         .to(".first h1", {
+          scale: 10,
+          ease: "bounce",
           scrollTrigger: {
             trigger: ".first",
-            start: "top center",
-            end: "=+1200",
-            markers: true,
+            start: "left 0",
+            end: "right 100",
             scrub: true,
+            pin: true,
+            // markers: true,
           },
-          ease: "power1",
-          scale: 10,
         })
         .to(".second h1", {
+          rotate: 360,
+          scale: 5,
+          ease: "linear",
           scrollTrigger: {
             trigger: ".second",
-            start: "top center",
-            end: "bottom bottom",
-            markers: true,
+            start: "<10%",
+            toggleActions: "play none none reset",
             scrub: true,
+            pin: true,
+            // markers: true,
           },
-          ease: "power1",
-          rotate: 360,
-          scale: 10,
         })
-        .fromTo(
-          ".third h1",
-          {
-            scrollTrigger: {
-              trigger: ".third",
-              start: "top center",
-              end: "=+900",
-              markers: true,
-              scrub: true,
-            },
-            ease: "elastic",
-            yoyo: true,
-            y: 0,
+        .from(".third h1", {
+          y: -180,
+          x: -300,
+          opacity: 0,
+          scale: 8,
+          duration: 0.5,
+          ease: "linear",
+          scrollTrigger: {
+            trigger: ".third",
+            start: "<10%",
+            scrub: true,
+            pin: true,
+            // markers: true,
           },
-          {
-            ease: "power1",
-            yoyo: true,
-            scale: 10,
-            y: "+=600",
-            repeat: Infinity,
-          }
-        )
-        .fromTo(
-          ".fourth h1",
-          {
-            scrollTrigger: {
-              trigger: ".third",
-              start: "top center",
-              end: "=+900",
-              markers: true,
-              scrub: true,
-            },
-            x: "-=3200",
-            duration: " <.8",
+        })
+        .to(".fourth h1", {
+          y: "200",
+          scale: 5,
+          ease: "power2",
+          scrollTrigger: {
+            trigger: ".fourth",
+            start: "center <0",
+            scrub: true,
+            pin: true,
+            // markers: true,
           },
-          {
-            x: 0,
-            repeat: Infinity,
-            ease: "back",
-          }
-        )
-        .fromTo(
-          ".fifth h1",
-          {
-            scrollTrigger: {
-              trigger: ".third",
-              start: "top center",
-              end: "=+900",
-              markers: true,
-              scrub: true,
-            },
-            y: "-=2000",
-            duration: "<.1",
+        })
+        .to(".fifth h1", {
+          y: "-200",
+          x: "200",
+          scale: 8,
+          ease: "bounce",
+          scrollTrigger: {
+            trigger: ".fifth",
+            start: "left <0",
+            scrub: true,
+            pin: true,
+            // markers: true,
           },
-          {
-            y: 0,
-            ease: "back",
-            repeat: Infinity,
-          }
-        );
-    }, app);
+        });
+    });
 
-    return () => ctx.revert();
+    return () => {
+      pin.revert();
+    };
   }, []);
+
   return (
-    <div className="App" ref={app}>
-      <div className="app-container">
-        <div className="box">
-          <div className="text">
-            <h1>Dhaniel</h1>
+    <div className="App">
+      <div ref={triggerRef}>
+        <div className="app-container" ref={sectionRef}>
+          <div className="box">
+            <div className="text">
+              <h1>Dhaniel</h1>
+            </div>
           </div>
-        </div>
-        <div className="box first">
-          <div className="text">
-            <h1>Adeola</h1>
+          <div className="box first">
+            <div className="text">
+              <h1>Adeola</h1>
+            </div>
           </div>
-        </div>
-        <div className="box second">
-          <div className="text">
-            <h1>Ronke</h1>
+          <div className="box second">
+            <div className="text">
+              <h1>Ronke</h1>
+            </div>
           </div>
-        </div>
-        <div className="box third">
-          <div className="text">
-            <h1>Adejoke</h1>
+          <div className="box third">
+            <div className="text">
+              <h1>Adejoke</h1>
+            </div>
           </div>
-        </div>
-        <div className="box fourth">
-          <div className="text">
-            <h1>Mofe</h1>
+          <div className="box fourth">
+            <div className="text">
+              <h1>Mofe</h1>
+            </div>
           </div>
-        </div>
-        <div className="box fifth">
-          <div className="text">
-            <h1>Joshua</h1>
+          <div className="box fifth">
+            <div className="text">
+              <h1>Joshua</h1>
+            </div>
           </div>
         </div>
       </div>
